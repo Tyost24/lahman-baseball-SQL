07c4c88d9559861f9c2c85481adb59c896eb8e09
TJ Yost <tjyost@TJs-MacBook-Pro.local>
2022-08-27 12:03:06 -0500

    Q7 B

--- Script/MyLahmanScript.sql
+++ Script/MyLahmanScript.sql
@@ -116,10 +116,18 @@ Having (sum(sb)+sum(cs)) >= 20
 order by Success_Stolen DESC
 
 
--- 7. From 1970 – 2016, what is the largest number of wins for a team that did not win the world series? What is the smallest number of wins for a team that did win the world series? Doing this will probably result in an unusually small number of wins for a world series champion – determine why this is the case. Then redo your query, excluding the problem year. How often from 1970 – 2016 was it the case that a team with the most wins also won the world series? What percentage of the time?
+-- 7.  A.From 1970 – 2016, what is the largest number of wins for a team that did not win the world series? B.What is the smallest number of wins for a team that did win the world series? Doing this will probably result in an unusually small number of wins for a world series champion – determine why this is the case. Then redo your query, excluding the problem year. C.How often from 1970 – 2016 was it the case that a team with the most wins also won the world series? What percentage of the time?
 
+--A. Seattle is the team with the largest number of wins that did not win the world series
 
-
+Select yearid,
+teamid,
+w,
+wswin
+from teams
+Where yearid Between 1970 and 2016
+And wswin = 'N'
+Order by w DESC
 
 
 
