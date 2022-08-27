c0950d22bddc46e28e2eec2a38b4814af2be1774
TJ Yost <tjyost@TJs-MacBook-Pro.local>
2022-08-27 11:48:03 -0500

    on Q7

--- Script/MyLahmanScript.sql
+++ Script/MyLahmanScript.sql
@@ -99,7 +99,7 @@ order by decade DESC;
 
 
 -- 6. Find the player who had the most success stealing bases in 2016, where success is measured as the percentage of stolen base attempts which are successful. (A stolen base attempt results either in a stolen base or being caught stealing.) Consider only players who attempted at least 20 stolen bases.
-
+ -- Chris Owings had the most at 91 Perceent
 
 
 Select
@@ -116,7 +116,7 @@ Having (sum(sb)+sum(cs)) >= 20
 order by Success_Stolen DESC
 
 
-
+-- 7. From 1970 – 2016, what is the largest number of wins for a team that did not win the world series? What is the smallest number of wins for a team that did win the world series? Doing this will probably result in an unusually small number of wins for a world series champion – determine why this is the case. Then redo your query, excluding the problem year. How often from 1970 – 2016 was it the case that a team with the most wins also won the world series? What percentage of the time?
 
 
 
